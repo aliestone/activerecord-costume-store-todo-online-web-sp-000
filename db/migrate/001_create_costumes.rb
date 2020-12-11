b79bb9d3bf95cb5d7eb72f68ab866f400ea763a7
Alie Stone <helloaliestone@gmail.com>
2020-12-11 00:32:10 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,12 +1,12 @@
 class CreateCostumes < ActiveRecord::Migration[5.1]
 
-  def change
-    create_table :costumes do |t|
-      t.string :name
-      t.integer :price
-      t.string :size
-      t.string :image_url
-      t.timestamps null: false
+def change
+  create_table :costumes do |t|
+    t.string :name
+    t.integer :price
+    t.string :size
+    t.string :image_url
+    t.timestamps null: false
   end
 end
 end
